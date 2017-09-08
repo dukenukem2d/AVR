35b0cdec50fc4347ab635f553d4091f1bcb70c6a
DukeParrot <glitchme4@gmail.com>
2017-09-08 12:15:03 +0300

    Add MakeFile to HumanSensor's project

--- attiny2313/HumanSensor/HumanSensor.cpp
+++ attiny2313/HumanSensor/HumanSensor.cpp
@@ -1,32 +1,3 @@
-/* Pinouts ATTINY2313
-
-                                       + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +
-                   1    PA2     +   (PCINT10/RESET/dW)                                                                      +    VCC     20
-                                       +                                                                                                                      +
-                   2    PD0     +   (PCINT11/RxD)                               (USCK/SCL/SCK/PCINT7)  +    PB7      19
-                                       +                                                                                                                      +
-                   3    PD1     +   (PCINT12/TxD)                                            (MISO/DO/PCINT6)  +   PB6     18
-                                       +                                                                                                                      +
-                   4    PA1     +   (PCINT9/XTAL2)                                  (MOSI/DI/SDA/PCINT5)  +    PB5    17
-                                       +                                                                                                                      +
-                   5    PA0     +   (PCINT8/CLKI/XTAL1 )                                      (OC1B/PCINT4)  +   PB4    16
-                                       +                                                                                                                       +
-                   6    PD2     +   (PCINT13/CKOUT/XCK/INT0 )                       (OC1A/PCINT3)  +   PB3    15
-                                       +                                                                                                                       +
-                   7    PD3     +   (PCINT14/INT1)                                                   (OC0A/PCINT2)  +   PB2    14
-                                       +                                                                                                                       +
-                   8    PD4     +   (PCINT15/T0)                                                        (AIN1/PCINT1)   +   PB1    13
-                                       +                                                                                                                       +
-                   9    PD5     +   (PCINT16/OC0B/T1)                                            (AIN0/PCINT0)  +   PB0    12
-                                       +                                                                                                                       +
-                  10   GND   +                                                                                       (ICPI/PCINT17)  +   PD6     11
-                                       + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +
-
-
-
-
- */
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/iotn2313.h>
